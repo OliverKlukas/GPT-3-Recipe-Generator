7e2795c07574149db2eec685cd79227a8cf72b6e
Oliver Klukas <oliverklukas@t-online.de>
2021-04-17 11:12:28 +0200

    Search function working

--- recipe_generator/lib/discovery.dart
+++ recipe_generator/lib/discovery.dart
@@ -8,7 +8,12 @@ class DiscoveryPage extends StatefulWidget {
 }
 
 class _DiscoveryPageState extends State<DiscoveryPage> {
-  List<Recipe> recipes = [
+
+  // Editing controller for search
+  TextEditingController editingController = TextEditingController();
+
+  // Recipe static dataset
+  List<Recipe> allRecipes = [
     Recipe(name: "Spaghetti Bolognese", imageURL: "https://img.chefkoch-cdn.de/rezepte/807111184564987/bilder/668266/crop-960x720/spaghetti-bolognese.jpg"),
     Recipe(name: "Pancakes", imageURL: "https://www.einfachbacken.de/sites/einfachbacken.de/files/styles/full_width_tablet_4_3/public/2020-08/american_pancakes.jpg?h=4521fff0&itok=yvzcMTED"),
     Recipe(name: "Indian Curry",imageURL: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/31/1501791674-delish-chicken-curry-horizontal.jpg?crop=1.00xw:0.750xh;0,0.159xh&resize=1200:*"),
@@ -22,6 +27,40 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
     Recipe(name: "Hamburger",  imageURL: "https://www.kuechengoetter.de/uploads/media/630x630/09/24729-hamburger-zum-selber-bauen.jpg?v=1-0"),
     Recipe(name: "Cheese Cake",  imageURL: "https://www.einfachbacken.de/sites/einfachbacken.de/files/styles/full_width_tablet_4_3/public/2020-03/klassischer_kasekuchen_2.jpg?h=4521fff0&itok=-W9far_5"),
   ];
+
+  // Dynamic recipe list for search
+  var dispRecipes = <Recipe>[];
+
+  // Initialize recipes
+  @override
+  void initState() {
+    dispRecipes.addAll(allRecipes);
+    super.initState();
+  }
+
+  // Search functionality
+  void filterSearchResults(String query) {
+    List<Recipe> dummySearchList = <Recipe>[];
+    dummySearchList.addAll(allRecipes);
+    if(query.isNotEmpty) {
+      List<Recipe> dummyListData = <Recipe>[];
+      dummySearchList.forEach((item) {
+        if(item.contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        dispRecipes.clear();
+        dispRecipes.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        dispRecipes.clear();
+        dispRecipes.addAll(allRecipes);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +84,10 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
             Padding(
               padding: EdgeInsets.only(top: 16,left: 16,right: 16),
               child: TextField(
+                onChanged: (value) {
+                  filterSearchResults(value);
+                },
+                controller: editingController,
                 decoration: InputDecoration(
                   hintText: "Search...",
                   hintStyle: TextStyle(color: Colors.grey.shade600),
@@ -62,14 +105,14 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
               ),
             ),
             ListView.builder(
-              itemCount: recipes.length,
+              itemCount: dispRecipes.length,
               shrinkWrap: true,
               padding: EdgeInsets.only(top: 16),
               physics: NeverScrollableScrollPhysics(),
               itemBuilder: (context, index){
                 return RecipesList(
-                  name: recipes[index].name,
-                  imageURL: recipes[index].imageURL,
+                  name: dispRecipes[index].name,
+                  imageURL: dispRecipes[index].imageURL,
                 );
               },
             ),
--- recipe_generator/lib/recipeModel.dart
+++ recipe_generator/lib/recipeModel.dart
@@ -4,4 +4,8 @@ class Recipe{
   String name;
   String imageURL;
   Recipe({required this.name,required this.imageURL});
+
+  bool contains(String query) { // make async if you don't want immediate change
+    return name.toLowerCase().contains(query.toLowerCase());
+  }
 }
