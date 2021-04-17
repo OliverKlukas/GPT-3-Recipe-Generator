c732c79a418f85e339cc03f8f3a6838a3e191621
David Stiftl <david.stiftl@gmx.de>
2021-04-18 01:01:08 +0200

    actually scrollable recipe

--- recipe_generator/lib/views/recipeDetail.dart
+++ recipe_generator/lib/views/recipeDetail.dart
@@ -12,7 +12,6 @@ class RecipeDetailApp extends StatefulWidget {
 }
 
 class _RecipeDetailAppState extends State<RecipeDetailApp> {
-
   late Future<String> futureFullRecipe;
 
   @override
@@ -31,130 +30,127 @@ class _RecipeDetailAppState extends State<RecipeDetailApp> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: Column(children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DefaultTextStyle.merge(
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w800,
-                      letterSpacing: 0.5,
-                      fontSize: 16,
-                      height: 2,
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                DefaultTextStyle.merge(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800,
+                    letterSpacing: 0.5,
+                    fontSize: 16,
+                    height: 2,
+                  ),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.wb_sunny, color: Colors.orangeAccent),
+                            Text('light'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.timer, color: Colors.black87),
+                            Text('1 hr'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.people, color: Colors.blueGrey),
+                            Text('4-6'),
+                          ],
+                        ),
+                      ],
                     ),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: [
-                              Icon(Icons.wb_sunny, color: Colors.orangeAccent),
-                              Text('light'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Icon(Icons.timer, color: Colors.black87),
-                              Text('1 hr'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Icon(Icons.people, color: Colors.blueGrey),
-                              Text('4-6'),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ]),
-            Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+                  ),
+                )
+              ]),
+              Divider(),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 Column(
                   children: [
                     Text(
                       "Ingredients",
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
                     ),
                   ],
                 ),
-              ]
-            ),
-            Row (
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(3.0),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        FutureBuilder<String>(
-                          future: futureFullRecipe,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              String fullText = snapshot.data!;
-                              List<String> response = parseIngredientsAndDirections(fullText);
-                              return Text(
-                                response[0],
-                                style: TextStyle(fontSize: 16),
-                              );
-                            } else if (snapshot.hasError) {
-                              return Text("${snapshot.error}");
-                            }
-                            // By default, show a loading spinner.
-                            return CircularProgressIndicator();
-                          },
-                        )
-                      ]
+              ]),
+              SingleChildScrollView(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  Padding(
+                    padding: const EdgeInsets.all(3.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          FutureBuilder<String>(
+                            future: futureFullRecipe,
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                String fullText = snapshot.data!;
+                                List<String> response =
+                                    parseIngredientsAndDirections(fullText);
+                                return Text(
+                                  response[0],
+                                  style: TextStyle(fontSize: 16),
+                                );
+                              } else if (snapshot.hasError) {
+                                return Text("${snapshot.error}");
+                              }
+                              // By default, show a loading spinner.
+                              return CircularProgressIndicator();
+                            },
+                          )
+                        ]),
                   ),
+                ]),
+              ),
+              Divider(),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Column(
+                  children: [
+                    Text(
+                      "Directions",
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
-              ]
-            ),
-            Divider(),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              ]),
+              Column(
                 children: [
-                  Column(
-                    children: [
-                      Text(
-                        "Directions",
-                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                ]
-            ),
-            Column(
-              children: [
-                FutureBuilder<String>(
-                  future: futureFullRecipe,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      String fullText = snapshot.data!;
-                      List<String> response = parseIngredientsAndDirections(fullText);
-                      return Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Text(
-                          response[1],
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-                    // By default, show a loading spinner.
-                    return CircularProgressIndicator();
-                  },
-                )
-              ],
-            ),
-          ]),
+                  FutureBuilder<String>(
+                    future: futureFullRecipe,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        String fullText = snapshot.data!;
+                        List<String> response =
+                            parseIngredientsAndDirections(fullText);
+                        return Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text(
+                            response[1],
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+                      // By default, show a loading spinner.
+                      return CircularProgressIndicator();
+                    },
+                  )
+                ],
+              ),
+            ]),
+          ),
         ));
   }
 }
