70a16042d4a08db1d79744494be75fe7f84d2a0f
Christopher Schütz <christopher.schuetz@tum.de>
2021-04-18 10:53:41 +0200

    remove debug banner and tests

--- recipe_generator/lib/main.dart
+++ recipe_generator/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
+      debugShowCheckedModeBanner: false,
       home: MyStatefulWidget(),
     );
   }
